4b4761860c9a2be4dfea76582a6b47bcc24e8359
openaki-dev <insight0312@gmail.com>
2024-02-29 12:13:15 +0900

    commit

--- app3.py
+++ app3.py
@@ -1,43 +1,31 @@
 # ----- 코드 정의 ------
 class member:
-    name = ''
-    username = ''
-    password = ''
-
     def __init__(self, name, username, password):
         self.name = name
         self.username = username
         self.password = password
-        print('어렵다 과제')
 
     def display(self):
         print(f"회원이름 : {self.name} 아이디: {self.username}")
 
 
-class Post:
-    title = ''
-    content = ''
-    auther = {{member.name}}
-
+class post:
     def __init__(self, title, content, auther):
         self.title = title
         self.content = content
         self.auther = auther
-        print('여기까진 이해 가능')
+
+     # 이 부분은 잘 모르겠다. class member 처럼 display를 써주어야하는 것 같은데 어떤 걸 써야하는지 아직 어렵다.
 
 
-# 회원인스턴스
-    m1 = member('강아지', '미루', '0000')
-    m2 = member('고양이', '루피', '0000')
-    m3 = member('토끼', '토롱이', '0000')
-    # m1 = self
-    # m1.name = 강아지
-    # m1.username = '미루'
-    # m1.password = '0000'
+# 회원인스턴스 m1 당 네줄로 썼다가 구글링 해보고 더 알아보기 쉽게 수정하였다.
+# m1=self 강아지=name 미루=username 0000=password
+m1 = member('강아지', '미루', '0000')
+m2 = member('고양이', '루피', '0000')
+m3 = member('토끼', '토롱이', '0000')
 
-
-# ----- 코드 실행 ------
 members = []
+
 members.append(m1)
 members.append(m2)
 members.append(m3)
@@ -48,4 +36,28 @@ for member in members:
 
 posts = []
 
-# TODO : 코드 구현이 필요합니다.
+for member in members:
+    print(member.name)
+
+p1 = post('첫번째 글', '첫번째 내용', m1.name)
+p2 = post('두번째 글', '두번째 내용', m1.name)
+p3 = post('세번째 글', '세번째 내용', m1.name)
+posts.append(p1)
+posts.append(p2)
+posts.append(p3)
+
+p4 = post('네번째 글', '네번째 내용', m2.name)
+p5 = post('다섯번째 글', '다섯번째 내용', m2.name)
+p6 = post('여섯번째 글', '여섯번째 내용', m2.name)
+posts.append(p4)
+posts.append(p5)
+posts.append(p6)
+
+p7 = post('일곱번째 글', '일곱번째 내용', m3.name)
+p8 = post('여덟번째 글', '여덟번째 내용', m3.name)
+p9 = post('아홉번째 글', '아홉번째 내용', m3.name)
+posts.append(p7)
+posts.append(p8)
+posts.append(p9)
+
+print(posts)
